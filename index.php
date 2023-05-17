1bd5c58d02ccdd2033c2b0a24c4953ede8de4014
shutaro18 <cp.aigis18@gmail.com>
2023-05-17 11:45:03 +0900

    最初のコミット

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>掲示板</title>
+</head>
+<body>
+    <header>
+        <h1 class="title">掲示板</h1>
+        <hr>
+    </header>
+
+    <!-- スレッドエリア　-->
+    <div class="threadWrapper">
+        <div class="childWrapper">
+            <div class="threadTitle">
+                <span>[タイトル]</span>
+                <h1>掲示板作ってみた</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
